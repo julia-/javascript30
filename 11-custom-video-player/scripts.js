5e863b16a37cb37db0dbef2af40bdd930696e6f6
julia- <julia.ryanx@gmail.com>
2017-12-16 17:56:58 +1100

    fix const skipButtons typo

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -5,7 +5,7 @@ const video = player.querySelector('.video');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
-const skipButton = player.querySelectorAll('[data-skip]');
+const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 // build functions
