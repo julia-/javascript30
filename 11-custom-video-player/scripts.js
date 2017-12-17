e5c3463c45b66bb04af74acad3f4371830912f1b
julia- <julia.ryanx@gmail.com>
2017-12-17 22:39:41 +1100

    update video constant selector

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -1,6 +1,6 @@
 // get elements
 const player = document.querySelector('.player');
-const video = player.querySelector('.video');
+const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
