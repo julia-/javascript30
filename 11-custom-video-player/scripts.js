6ee415108e98f5a7f893d8ff0223f8d6e9a9a89d
julia- <julia.ryanx@gmail.com>
2017-12-17 22:44:38 +1100

    skip backwards or forwards when skip buttons are clicked

--- 11-custom-video-player/scripts.js
+++ 11-custom-video-player/scripts.js
@@ -21,8 +21,13 @@ function updateButton() {
   toggle.textContent = icon;
 }
 
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
 // hook up event listeners
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
